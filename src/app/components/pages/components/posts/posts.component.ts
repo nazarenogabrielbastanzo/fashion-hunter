d5b0f6d81f81bf1c9177751a13a03795e37cfe0a
Nazareno Gabriel Bastanzo <b.nazarenogabriel@gmail.com>
2022-05-02 13:01:56 -0300

    limpieza de codigo

--- src/app/components/pages/components/posts/posts.component.ts
+++ src/app/components/pages/components/posts/posts.component.ts
@@ -1,10 +1,5 @@
-import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
-import * as moment from 'moment';
-import { BehaviorSubject, Observable, tap } from 'rxjs';
-import { HttpConfigService } from 'src/app/services/http-config.service';
-import { environment } from 'src/environments/environment';
-import { ModalCommentsComponent } from '../modal-comments/modal-comments.component';
+import { Component, OnInit } from '@angular/core';
+import { Observable, tap } from 'rxjs';
 import { PostService } from '../../../../services/post.service';
 
 @Component({
@@ -13,26 +8,19 @@ import { PostService } from '../../../../services/post.service';
   styleUrls: ['./posts.component.css'],
 })
 export class PostsComponent implements OnInit {
-  posts: any;
   posts$!: Observable<any[]>;
 
-  constructor(
-    private postSvc: PostService
-  ) {
+  constructor(private postSvc: PostService) {
     this.postSvc
       .getPosts()
       .pipe(
         tap((res: any) => {
           console.log(res.data.resolvedPost);
-          this.posts = res.data.resolvedPost;
-          // this.postSvc.postsSource.next(res.data.resolvedPost);
           this.posts$ = this.postSvc.posts$;
         })
       )
       .subscribe();
   }
 
-  ngOnInit(): void {
-
-  }
+  ngOnInit(): void {}
 }
